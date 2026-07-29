3c4b779ac387366b547a582ae6cbe59d0f048e82
Eric Kowalewski <eric@thebeartraxs.com>
2026-07-29 12:57:50 -0400

    chore(release): v0.1.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## [0.1.3] - 2026-07-29
+
+### Changes
+* chore(release): v0.1.2 (bd85347)
+
+
 ## [0.1.2] - 2026-06-17
 
 ### Changes
--- rumble.php
+++ rumble.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Version: 0.1.2
+ * Version: 0.1.3
 Plugin Name: Rumble
 Description: Kiosk quoting for Bear Traxs with quote accept + product creation.
 Author: Bear Traxs
@@ -13,14 +13,15 @@ if (!defined('ABSPATH')) exit;
 
 
 
-define('RUMBLE_VERSION', '0.1.2');
+define('RUMBLE_VERSION', '0.1.3');
 define('RUMBLE_PATH', plugin_dir_path(__FILE__));
 define('RUMBLE_URL', plugin_dir_url(__FILE__));
 define('RUMBLE_SLUG', plugin_basename(__FILE__));
 
+require_once RUMBLE_PATH.'includes/helpers.php';
+require_once RUMBLE_PATH.'includes/settings.php';
 require_once RUMBLE_PATH.'includes/roles.php';
 require_once RUMBLE_PATH.'includes/cpt.php';
-require_once RUMBLE_PATH.'includes/helpers.php';
 require_once RUMBLE_PATH.'includes/logs.php';
 require_once RUMBLE_PATH.'includes/freshbooks.php';
 require_once RUMBLE_PATH.'includes/shipstation.php';
@@ -28,6 +29,7 @@ require_once RUMBLE_PATH.'includes/admin.php';
 require_once RUMBLE_PATH.'includes/accept.php';
 require_once RUMBLE_PATH.'includes/assets.php';
 require_once RUMBLE_PATH.'includes/ajax.php';
+require_once RUMBLE_PATH.'includes/email-intake.php';
 require_once RUMBLE_PATH.'includes/email.php';
 require_once RUMBLE_PATH.'includes/product.php';
 require_once RUMBLE_PATH.'includes/woocommerce_bridge.php';
